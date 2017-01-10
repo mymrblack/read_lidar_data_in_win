8be2dabf6c077100907b0d3e1e455f33dd219999
mymrblack <mymrblack@live.com>
2017-01-10 16:05:15 +0800

    add the define of DATA_LEN

--- read_lidar_file_in_win/read_lidar_file_proj.c
+++ read_lidar_file_in_win/read_lidar_file_proj.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-    
+
+/************** Choose Mode *************/
+#define IMODE 1
+/****************************************/
 
 #define MAXLEN 1024
-#define IMODE 1
 #define FIFO_SIZE           8192
+#define FIFO_NUM_EACH_PAGE  100
+#define READ_SIZE   FIFO_SIZE * FIFO_NUM_EACH_PAGE
+
 #ifdef IMODE
 #define DATA_WORDS	11
+#define DATA_LEN    100
 #else
 #define DATA_WORDS	5
+#define DATA_LEN    50 //need to be test.
 #endif
 
 #ifdef IMODE
@@ -48,11 +54,13 @@ typedef	struct{
 	int microsec;
 } Time_Data;
 
-Lidar_Data lidarData[FIFO_SIZE] = {{0}};
-Time_Data gpsData[FIFO_SIZE] = {{0}};
+Lidar_Data lidarData[READ_SIZE] = {{0}};
+Time_Data gpsData[READ_SIZE] = {{0}};
+unsigned char buf[DATA_LEN * READ_SIZE];
 
 void FIFO_printData(Lidar_Data *data, int range, int step);
 void Lidar_printData(Lidar_Data *data, Time_Data *gps, int range, int step);
+void Lidar_printDataToString(Lidar_Data *data, Time_Data *gps, char *buf);
 void GPS_timeDataDecode(Time_Data *timeData);
     
 int main(int argc, char *argv[]){
@@ -64,35 +72,29 @@ int main(int argc, char *argv[]){
 
     FILE * outfile, *infile;
     
-  //  outfile = fopen(argv[2], "wb" );
+    outfile = fopen(argv[2], "wb" );
 	infile = fopen(argv[1], "rb");
     
-   // unsigned char buf[MAXLEN];
-    
- /*   if( outfile == NULL || infile == NULL ){
+    if( outfile == NULL || infile == NULL ){
         printf("%s, %s",argv[1],"not exit/n");
         exit(1);
-    }   */
+    }   
     
-    int rc;
-    
-/*    while( (rc = fread(buf,sizeof(unsigned char), MAXLEN,infile)) != 0 ){
-        fwrite( buf, sizeof( unsigned char ), rc, outfile );
-    }*/
-    fread(lidarData, sizeof(int), DATA_WORDS*FIFO_SIZE, infile);
+    fread(lidarData, sizeof(int), DATA_WORDS * FIFO_SIZE * FIFO_NUM_EACH_PAGE, infile);
 
-    //FIFO_printData(lidarData, FIFO_SIZE, 300);
 	int i;
-	for (i = 0; i < FIFO_SIZE; i++){
+	for (i = 0; i < FIFO_SIZE * FIFO_NUM_EACH_PAGE; i++){
        gpsData[i].timeData1 = lidarData[i].gps1;
        gpsData[i].timeData2 = lidarData[i].gps2;
        GPS_timeDataDecode(&gpsData[i]);
     }
-    Lidar_printData(lidarData, gpsData, FIFO_SIZE, 300);
+ //   Lidar_printData(lidarData, gpsData FIFO_SIZE, 300);
+
+    Lidar_printDataToString(lidarData, gpsData, buf);
+    fwrite( buf, sizeof( unsigned char ), sizeof(buf), outfile );
 
     fclose(infile);
-
- //   fclose(outfile);
+    fclose(outfile);
 
     system("PAUSE");
 
@@ -127,15 +129,13 @@ void GPS_timeDataDecode(Time_Data *timeData){
 	timeData->second = ((timeData->timeData2) >> 10 )& 0x3F;
 	timeData->millisec = (timeData->timeData1) & 0x3ff;
 	timeData->microsec = (timeData->timeData2) & 0x3ff;
-	return;
 }
 
 void Lidar_printData(Lidar_Data *data, Time_Data *gps, int range, int step){
     int i;
     #ifdef IMODE
     for(i = 0; i < range; i+= step){
-        printf("triTimes:%4d\t Time:%4d/%2d/%2d-%2d:%2d:%2d-%3d:%3d\tch1:%8d\tch2:%8d\tch3:%8d\tch4:%8d\tch5:%8d\tch6:%8d\tch7:%8d\tch8:%8d\r\n",
-        		(&data[i])->triTimes, 
+        printf("triTimes:%04d\t Time:%04d/%02d/%02d-%02d:%02d:%02d-%03d:%03d\tch1:%06d\tch2:%06d\tch3:%06d\tch4:%06d\tch5:%06d\tch6:%06d\tch7:%06d\tch8:%06d\r\n",
                 (&gps[i])->year, (&gps[i])->month, (&gps[i])->day,
                 (&gps[i])->hour, (&gps[i])->minute, (&gps[i])->second,
                 (&gps[i])->millisec, (&gps[i])->microsec,
@@ -145,7 +145,45 @@ void Lidar_printData(Lidar_Data *data, Time_Data *gps, int range, int step){
     }
     #else
     for(i = 0; i < range; i+= step){
-        printf("triTimes:%4d\t Time:%4d/%2d/%2d-%2d:%2d:%2d-%3d:%3d\tch1:%8d\tch2:%8d\r\n),
+        printf("triTimes:%04d\t Time:%04d/%02d/%02d-%02d:%02d:%02d-%03d:%03d\tch1:%08d\tch2:%08d\r\n)",
+        		(&data[i])->triTimes, 
+                (&gps[i])->year, (&gps[i])->month, (&gps[i])->day,
+                (&gps[i])->hour, (&gps[i])->minute, (&gps[i])->second,
+                (&gps[i])->millisec, (&gps[i])->microsec,
+        		(&data[i])->ch1, (&data[i])->ch2);
+    }
+    #endif
+}
+
+#ifdef IMODE
+#define DATA_FORMAT "%04d/%02d/%02d-%02d:%02d:%02d-%03d:%03d\t%6d\t%6d\t%6d\t%6d\t%6d\t%6d\t%6d\t%6d\n"
+#define TITLE_FORMAT "time\tch1\tch2\tch3\tch4\tch5\tch6\tch7\tch8\n"
+#else
+#define DATA_FORMAT "%04d/%02d/%02d-%02d:%02d:%02d-%03d:%03d\tch1\t%8d\tch2\t%8d\n" 
+#define TITLE_FORMAT "time\tch1\tch2\n"
+#endif
+
+void Lidar_printDataToString(Lidar_Data *data, Time_Data *gps, char *buf){
+    int i;
+    int line_len = 0;
+	int title_len = 0;
+	line_len = strlen(DATA_FORMAT) + 16;
+	title_len = strlen(TITLE_FORMAT);
+
+    sprintf(buf, TITLE_FORMAT);
+    #ifdef IMODE
+    for(i = 1; i < READ_SIZE; i++){
+        sprintf(buf + title_len + i*(line_len), DATA_FORMAT,
+                (&gps[i])->year, (&gps[i])->month, (&gps[i])->day,
+                (&gps[i])->hour, (&gps[i])->minute, (&gps[i])->second,
+                (&gps[i])->millisec, (&gps[i])->microsec,
+				(&data[i])->ch1, (&data[i])->ch2, (&data[i])->ch3,
+				(&data[i])->ch4, (&data[i])->ch5, (&data[i])->ch6,
+				(&data[i])->ch7, (&data[i])->ch8);
+    }
+    #else
+    for(i = 0; i < READ_SIZE; i++){
+        sprintf(buf + i*(line_len + 4), DATA_FORMAT,
         		(&data[i])->triTimes, 
                 (&gps[i])->year, (&gps[i])->month, (&gps[i])->day,
                 (&gps[i])->hour, (&gps[i])->minute, (&gps[i])->second,
